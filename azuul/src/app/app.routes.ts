e5b08171672f2fc99a8b189925f2e0e0f9b42ec3
saidamst2003 <saida2003mst@gmail.com>
2025-07-04 15:13:53 +0100

    j'ai creer page de pause  cafee

--- azuul/src/app/app.routes.ts
+++ azuul/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Atelier } from './pages/atelier/atelier';
+import { PauseCafe } from './pages/pause-cafee/pause-cafee';
 
 export const routes: Routes = [
-  { path: '', component: Atelier }
+  { path: '', component: Atelier },
+  { path: 'atelier', component: Atelier },
+  { path: 'pause-cafee', component: PauseCafe }
 ];
