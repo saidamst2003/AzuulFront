4bdc50afb40ae3dc904e3296a7fdfbba8f7dcee7
saidamst2003 <saida2003mst@gmail.com>
2025-07-21 09:42:34 +0100

    j'ai modifier dans ts de atelier

--- azuul/src/app/pages/atelier/atelier.ts
+++ azuul/src/app/pages/atelier/atelier.ts
@@ -1,13 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Atelier, AtelierService } from '../../services/atelier.service';
+import { AuthService } from '../../services/auth';
 import { Navbar } from '../../layout/navbar/navbar';
 import { Footer } from '../../layout/footer/footer';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-atelier',
-  imports: [Navbar, Footer],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, Navbar, Footer],
   templateUrl: './atelier.html',
   styleUrl: './atelier.css'
 })
-export class Atelier {
+export class Atelier implements OnInit {
+  ateliers: Atelier[] = [];
+  atelierForm: FormGroup;
+  editingAtelier: Atelier | null = null;
+  showForm = false;
+  error = '';
+  isAdmin = false;
 
+  constructor(
+    private atelierService: AtelierService,
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) {
+    this.atelierForm = this.fb.group({
+      nom: ['', Validators.required],
+      description: ['', Validators.required],
+      date: ['', Validators.required],
+      heure: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.loadAteliers();
+    this.authService.user$.subscribe((user: User | null) => {
+      this.isAdmin = !!user && user.role === 'ADMIN';
+    });
+  }
+
+  loadAteliers() {
+    this.atelierService.getAllAteliers().subscribe({
+      next: (data) => this.ateliers = data,
+      error: (err) => this.error = 'Erreur lors du chargement des ateliers.'
+    });
+  }
+
+  openCreateForm() {
+    this.editingAtelier = null;
+    this.atelierForm.reset();
+    this.showForm = true;
+  }
+
+  openEditForm(atelier: Atelier) {
+    this.editingAtelier = atelier;
+    this.atelierForm.patchValue(atelier);
+    this.showForm = true;
+  }
+
+  submitForm() {
+    if (!this.atelierForm.valid) {
+      this.error = 'Veuillez remplir tous les champs.';
+      return;
+    }
+
+    const action = this.editingAtelier
+      ? this.atelierService.updateAtelier(this.editingAtelier.id!, this.atelierForm.value)
+      : this.atelierService.createAtelier(this.atelierForm.value);
+
+    action.subscribe({
+      next: () => {
+        this.loadAteliers();
+        this.cancel();
+      },
+      error: (err) => this.error = `Erreur lors de ${this.editingAtelier ? 'la modification' : 'la création'}.`
+    });
+  }
+
+  deleteAtelier(id: number) {
+    if (confirm('Voulez-vous vraiment supprimer cet atelier ?')) {
+      this.atelierService.deleteAtelier(id).subscribe({
+        next: () => this.loadAteliers(),
+        error: () => this.error = 'Erreur lors de la suppression.'
+      });
+    }
+  }
+
+  cancel() {
+    this.showForm = false;
+    this.editingAtelier = null;
+    this.atelierForm.reset();
+    this.error = '';
+  }
 }
