274f189a81312de98d36e5dc82dcfbf3bfa98cf4
saidamst2003 <saida2003mst@gmail.com>
2025-07-24 16:39:50 +0100

    j'ai creer image-uploader

---
+++ azuul/src/app/pages/image-uploader/image-uploader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-uploader',
+  imports: [],
+  templateUrl: './image-uploader.html',
+  styleUrl: './image-uploader.css'
+})
+export class ImageUploader {
+
+}
